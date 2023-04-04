a45efd9b735b97746e839f13139ad4261488acd8
ddsff <2843223265@qq.com>
2023-04-04 23:01:59 +0800

    15

---
+++ leetcode.c
@@ -0,0 +1,5 @@
+//
+// Created by 28432 on 2023/4/4.
+//
+
+#include "leetcode.h"
---
+++ leetcode.h
@@ -0,0 +1,8 @@
+//
+// Created by 28432 on 2023/4/4.
+//
+
+#ifndef CLION_LEETCODE_H
+#define CLION_LEETCODE_H
+
+#endif //CLION_LEETCODE_H
